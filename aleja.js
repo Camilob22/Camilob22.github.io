bfa32044dfeabd83c9fc55d98d0b09972e949ffe
Camilob22 <123681254+Camilob22@users.noreply.github.com>
2025-03-13 15:58:58 -0500

    Add files via upload

---
+++ aleja.js
@@ -0,0 +1,20 @@
+function openNewPage(){
+    window.location.href = "secondPage.html";
+}
+
+function openMoments(){
+    window.location.href = "momentsAli.html";
+}
+
+function openMessage(){
+    window.location.href = "messageAli.html";
+}
+
+function openPlaylist(){
+    window.location.href = "playlistAli.html";
+}
+
+
+function back() {
+    window.history.back();
+}
